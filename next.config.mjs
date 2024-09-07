dcd4c4a5849d712f6a01c2d101d10766d4c53dd9
platonvin <platonvin@gmail.com>
2024-09-07 19:10:50 +0300

    rollback for config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
  
-export default (phase) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    output: "export",
-  }
-  return nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
 }
+   
+export default nextConfig
